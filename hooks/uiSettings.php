bfe331aa000fd0fd62edd6e1c99bcd50b4fd7b47
Andrew Rivett <veggiefrog@gmail.com>
2015-03-29 08:26:05 -0400

    Added courses, instructors and locations to uiSettings

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -17,7 +17,7 @@ function ciniki_fatt_hooks_uiSettings($ciniki, $business_id, $args) {
 	$settings = array();
 
 	//
-	// Load the courses for the business
+	// Load the courses and instructors for the business
 	//
 	if( ($ciniki['business']['modules']['ciniki.fatt']['flags']&0x01) > 0 ) {
 		$strsql = "SELECT id, name "
@@ -36,6 +36,47 @@ function ciniki_fatt_hooks_uiSettings($ciniki, $business_id, $args) {
 		}
 		if( isset($rc['courses']) ) {
 			$settings['courses'] = $rc['courses'];
+		}
+
+		$strsql = "SELECT id, name "
+			. "FROM ciniki_fatt_instructors "
+			. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND status = 10 "
+			. "ORDER BY name "
+			. "";
+		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
+		$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'ciniki.fatt', array(
+			array('container'=>'instructors', 'fname'=>'id', 'name'=>'instructor',
+				'fields'=>array('id', 'name')),
+			));
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( isset($rc['instructors']) ) {
+			$settings['instructors'] = $rc['instructors'];
+		}
+	}
+
+	//
+	// Load the fatt locations for the business
+	//
+	if( ($ciniki['business']['modules']['ciniki.fatt']['flags']&0x04) > 0 ) {
+		$strsql = "SELECT id, name "
+			. "FROM ciniki_fatt_locations "
+			. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "AND status = 10 "
+			. "ORDER BY name "
+			. "";
+		ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
+		$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'ciniki.fatt', array(
+			array('container'=>'locations', 'fname'=>'id', 'name'=>'location',
+				'fields'=>array('id', 'name')),
+			));
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( isset($rc['locations']) ) {
+			$settings['locations'] = $rc['locations'];
 		}
 	}
 
