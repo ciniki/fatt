20c109b04492429954114459c31beaacc17ce2e2
Andrew Rivett <veggiefrog@gmail.com>
2018-11-07 23:50:05 -0500

    Removed register button

--- web/processRequest.php
+++ web/processRequest.php
@@ -251,7 +251,8 @@ function ciniki_fatt_web_processRequest(&$ciniki, $settings, $tnid, $args) {
 
         if( isset($course['offerings']) && count($course['offerings']) > 0 ) {
             foreach($course['offerings'] as $oid => $offering) {    
-                $course['offerings'][$oid]['edit_button'] = "<a href='{$base_url}/register/{$offering['uuid']}'>Register</a>";
+                $course['offerings'][$oid]['edit_button'] = "";
+//                $course['offerings'][$oid]['edit_button'] = "<a href='{$base_url}/register/{$offering['uuid']}'>Register</a>";
             }
             $page['blocks'][] = array('type'=>'pricetable', 'title'=>'Upcoming Courses', 
                 'headers'=>array('Date(s)', 'Time(s)', 'Location', 'Price', ''),
